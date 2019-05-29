e18292cbf2d4ecff5153bb4dd4de52239367c2d9
Rodrigo Dias <diasbass@hotmail.com>
2019-05-29 14:48:48 -0300

    Section 4.24 - Usando decorator @input

--- jedi-academy/src/app/app.component.ts
+++ jedi-academy/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "jad-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  luke = { name: "Luke", isJedi: true, temple: "Coruscant" };
+  leia = { name: "Leia", isJedi: false };
+  han = { name: "Han Solo", isJedi: false };
 }
